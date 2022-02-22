befabd3717cb379979a697bacb3cbd9c9137dd53
LucasThr <lucasthirion@live.fr>
2022-02-22 12:17:21 +0100

    first commit

--- src/views/CityWeather.js
+++ src/views/CityWeather.js
@@ -8,7 +8,10 @@ const CityWeather = (props) => {
   return (
     <main className="flex relative items-center flex-col h-screen bg-gray-800 dark:text-white text-black">
       <Navbar />
-      {city}
+      <div className="flex w-full justify-between px-5">
+        <h3 className="text-4xl">{city}</h3>
+        <button>Fav</button>
+      </div>
     </main>
   );
 };
